8440e03e784b1a872d340c3797b0bc99d44a2831
fraizis <fraizis@mail.ru>
2026-02-27 21:21:23 +0300

    test_work_1

--- mysql_course.py
+++ mysql_course.py
@@ -22,7 +22,6 @@
 # ) COMMENT 'пользователи';
 
 # 1 x 1
-
 # DROP TABLE IF EXISTS user_settings;
 # CREATE TABLE user_settings(
 #     user_id BIGINT UNSIGNED NOT NULL,
@@ -54,18 +53,167 @@
 
 
 # 1 x M
-
 # DROP TABLE IF EXISTS private_messages;
 # CREATE TABLE private_messages(
-#     id SERIAL,
+# 	id SERIAL,
 #     sender_id BIGINT UNSIGNED NOT NULL,
 #     receiver_id BIGINT UNSIGNED NOT NULL,
-#     media_type ENUM('text', 'video', 'audio', 'image')
-#     / media_type_id BIGINT UNSIGNED NOT NULL /
+#     reply_to_id BIGINT UNSIGNED NULL,
+#     media_type ENUM('text', 'video', 'audio', 'image'),
+# --     / media_type_id BIGINT UNSIGNED NOT NULL /,
 #     body TEXT,
 #     filename VARCHAR(200),
-#     created_at DATETIME DEFAULT NOW()
-#     FOREIGN KEY(sender_id) REFERENCES users(id),
-#     FOREIGN KEY(receiver_id) REFERENCES users(id)
+#     created_at DATETIME DEFAULT NOW(),
+#
+#     FOREIGN KEY (sender_id) REFERENCES users (id),
+#     FOREIGN KEY (receiver_id) REFERENCES users (id),
+#     FOREIGN KEY (reply_to_id) REFERENCES private_messages (id)
+# );
+#
+# -- ALTER TABLE user_settings ADD PRIMARY KEY (user_id);
+# -- ALTER TABLE user_settings MODIFY COLUMN user_id BIGINT UNSIGNED NOT NULL PRMARY KEY;
+#
+#
+# DROP TABLE IF EXISTS `groups`;
+# CREATE TABLE `groups` (
+# 	id SERIAL,
+# 	title VARCHAR(45),
+# 	icon VARCHAR(45),
+# 	invite_link VARCHAR(100),
+# 	settings JSON,
+# 	owner_user_id BIGINT UNSIGNED NOT NULL,
+# 	is_private BIT,
+# 	created_at DATETIME DEFAULT NOW(),
+#
+# 	FOREIGN KEY (owner_user_id) REFERENCES users (id)
+# );
+#
+#
+# DROP TABLE IF EXISTS `group_members`;
+# CREATE TABLE `group_members` (
+# 	`id` SERIAL,
+# 	`group_id` BIGINT UNSIGNED NOT NULL,
+# 	`user_id` BIGINT UNSIGNED NOT NULL,
+# 	`created_at` DATETIME DEFAULT NOW(),
+#
+# 	FOREIGN KEY (user_id) REFERENCES users (id),
+# 	FOREIGN KEY (group_id) REFERENCES `groups` (id)
+# );
+#
+#
+# DROP TABLE IF EXISTS `group_messages`;
+# CREATE TABLE `group_messages` (
+# 	id SERIAL,
+# 	group_id BIGINT UNSIGNED NOT NULL,
+#     sender_id BIGINT UNSIGNED NOT NULL,
+#     reply_to_id BIGINT UNSIGNED NULL,
+#     media_type ENUM('text', 'video', 'audio', 'image'),
+#     body TEXT,
+#     filename VARCHAR(100) NULL,
+#     created_at DATETIME DEFAULT NOW(),
+#
+#     FOREIGN KEY (sender_id) REFERENCES users(id),
+#     FOREIGN KEY (group_id) REFERENCES `groups`(id),
+# 	FOREIGN KEY (reply_to_id) REFERENCES group_messages (id)
+# );
 
+
+# DROP TABLE IF EXISTS channels;
+# CREATE TABLE channels (
+# 	id SERIAL,
+# 	title VARCHAR(45),
+# 	icon VARCHAR(45),
+# 	invite_link VARCHAR(100),
+# 	settings JSON,
+# 	owner_user_id BIGINT UNSIGNED NOT NULL,
+# 	is_private BIT,
+# 	created_at DATETIME DEFAULT NOW(),
+#
+# 	FOREIGN KEY (owner_user_id) REFERENCES users (id)
+# );
+#
+
+# DROP TABLE IF EXISTS channel_subscribers;
+# CREATE TABLE channel_subscribers (
+# 	channel_id BIGINT UNSIGNED NOT NULL,
+# 	user_id BIGINT UNSIGNED NOT NULL,
+# 	status ENUM('requested', 'joined', 'left'),
+# 	created_at DATETIME DEFAULT NOW(),
+# 	updated_at DATETIME ON UPDATE CURRENT_TIMESTAMP,
+#
+#   PRIMARY KEY(user_id, channel_id),
+# 	FOREIGN KEY (channel_id) REFERENCES channels (id),
+# 	FOREIGN KEY (user_id) REFERENCES users (id)
+# );
+
+
+# DROP TABLE IF EXISTS `channel_messages`;
+# CREATE TABLE `channel_messages` (
+# 	id SERIAL,
+# 	channel_id BIGINT UNSIGNED NOT NULL,
+#     sender_id BIGINT UNSIGNED NOT NULL,
+#     media_type ENUM('text', 'video', 'audio', 'image'),
+#     body TEXT,
+#     filename VARCHAR(100) NULL,
+#     created_at DATETIME DEFAULT NOW(),
+#
+#     FOREIGN KEY (sender_id) REFERENCES users (id),
+#     FOREIGN KEY (channel_id) REFERENCES `channels` (id)
+# );
+#
+#
+# DROP TABLE IF EXISTS `saved_messages`;
+# CREATE TABLE `saved_messages` (
+# 	id SERIAL,
+#     user_id BIGINT UNSIGNED NOT NULL,
+#     body TEXT,
+#     created_at DATETIME DEFAULT NOW(),
+#
+#     FOREIGN KEY (user_id) REFERENCES users (id)
+# );
+
+
+# DROP TABLE IF EXISTS reactions_list;
+# CREATE TABLE reactions_list (
+# 	id SERIAL,
+# 	code VARCHAR(1)
+# )DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci;
+#
+#
+# DROP TABLE IF EXISTS private_message_reactions;
+# CREATE TABLE private_message_reactions (
+# 	reaction_id BIGINT UNSIGNED NOT NULL,
+#     message_id BIGINT UNSIGNED NOT NULL,
+#     user_id BIGINT UNSIGNED NOT NULL,
+#     created_at DATETIME DEFAULT NOW(),
+#
+#     FOREIGN KEY (reaction_id) REFERENCES reactions_list (id),
+#     FOREIGN KEY (message_id) REFERENCES private_messages (id),
+#     FOREIGN KEY (user_id) REFERENCES users (id)
+# );
+#
+#
+# DROP TABLE IF EXISTS group_message_reactions;
+# CREATE TABLE group_message_reactions (
+# 	reaction_id BIGINT UNSIGNED NOT NULL,
+#     message_id BIGINT UNSIGNED NOT NULL,
+#     user_id BIGINT UNSIGNED NOT NULL,
+#     created_at DATETIME DEFAULT NOW(),
+#
+#     FOREIGN KEY (reaction_id) REFERENCES reactions_list (id),
+#     FOREIGN KEY (message_id) REFERENCES group_messages (id),
+#     FOREIGN KEY (user_id) REFERENCES users (id)
+# );
+#
+#
+# DROP TABLE IF EXISTS channel_message_reactions;
+# CREATE TABLE channel_message_reactions (
+# 	reaction_id BIGINT UNSIGNED NOT NULL,
+#     message_id BIGINT UNSIGNED NOT NULL,
+#     user_id BIGINT UNSIGNED NOT NULL,
+#     created_at DATETIME DEFAULT NOW(),
+#
+#     FOREIGN KEY (reaction_id) REFERENCES reactions_list (id),
+#     FOREIGN KEY (message_id) REFERENCES channel_messages (id),
+#     FOREIGN KEY (user_id) REFERENCES users (id)
 # );
